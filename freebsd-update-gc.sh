kamadak/freebsd-update-gc

commit 55f73d6534689f3b653b8ede09241c2c6aadb825

Add find-garbage command.

freebsd-update-gc.sh
@@ -107,20 +107,29 @@ check_if_last_rp() {
 # List object hashes.  Each hash is followed by the newest rollback point
 # that refers the object.
 get_newest_ref() {
-	local rp rollback_list
+	local rp rollback_list find_garbage
 
 	rollback_list=$1
+	find_garbage=$2
 
 	# Output newer rollback points first.  sort -u implies a stable sort,
 	# so the newest point is preserved.
 	{
-		cut -d"|" -f2 $DBDIR/tINDEX.present | sed 's/$/ tINDEX.present/'
+		# tag points to tINDEX.present, which is not in the "files".
+		#cut -d"|" -f5 $DBDIR/tag | sed 's/$/.gz tag/'
+		cut -d"|" -f2 $DBDIR/tINDEX.present | \
+		    sed 's/$/.gz tINDEX.present/'
 		for rp in $rollback_list; do
 			awk -F"|" -vrp="$rp" '
 				$2 != "L" && $2 != "d" && $2 != "-" {
-					print $7, rp;
+					print $7 ".gz", rp;
 				}' $DBDIR/$rp/INDEX-OLD $DBDIR/$rp/INDEX-NEW
 		done
+		if [ -n "$find_garbage" ]; then
+			# find is faster than "ls -f" on FreeBSD 10.0.
+			(cd $DBDIR/files && find . -mindepth 1 -maxdepth 1 | \
+			    sed 's|^\./||; s/$/ unref/')
+		fi
 	} | sort -u -k1,1
 }
 
@@ -217,9 +226,30 @@ remove() {
 	EOS
 }
 
+find_garbage() {
+	local newest_ref rp rollback_list
+
+	newest_ref=$1
+	rollback_list=$2
+
+	# Unreferenced files/*.
+	awk '/ unref$/ { print "files/" $1 }' <<-EOS
+		$newest_ref
+	EOS
+
+	# Unreferenced install.*.
+	{
+		for rp in $rollback_list; do
+			printf "%s ref\n" $rp
+		done
+		(cd $DBDIR && ls -df install.*)
+	} | sort -u -k1,1 | grep -v ' ref$' || true
+}
+
 usage() {
 	printf "usage: %s list\n" "$progname"
 	printf "       %s remove install.id\n" "$progname"
+	printf "       %s find-garbage\n" "$progname"
 	exit 1
 }
 
@@ -231,14 +261,19 @@ error() {
 if [ $# -eq 1 -a x"$1" = xlist ]; then
 	rollback_list=$(get_rollback_list)
 	pending_list=$(get_pending_list)
-	newest_ref=$(get_newest_ref "$pending_list $rollback_list")
+	newest_ref=$(get_newest_ref "$pending_list $rollback_list" "")
 	list "$rollback_list" "$pending_list" "$newest_ref"
 elif [ $# -eq 2 -a x"$1" = xremove ]; then
 	rollback_list=$(get_rollback_list)
 	pending_list=$(get_pending_list)
 	prev_rp=$(check_if_last_rp "$rollback_list" "$2")
-	newest_ref=$(get_newest_ref "$pending_list $rollback_list")
+	newest_ref=$(get_newest_ref "$pending_list $rollback_list" "")
 	remove "$newest_ref" "$2" "$prev_rp"
+elif [ $# -eq 1 -a x"$1" = xfind-garbage ]; then
+	rollback_list=$(get_rollback_list)
+	pending_list=$(get_pending_list)
+	newest_ref=$(get_newest_ref "$pending_list $rollback_list" find_garbage)
+	find_garbage "$newest_ref" "$pending_list $rollback_list"
 else
 	usage
 fi

freebsd-update-gc.8
@@ -24,7 +24,7 @@
 .\" OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
 .\" SUCH DAMAGE.
 .\"
-.Dd November 27, 2014
+.Dd November 30, 2014
 .Dt FREEBSD-UPDATE-GC 8
 .Os
 .\" ----------------------------------------------------------------
@@ -37,6 +37,8 @@
 .Cm list
 .Nm
 .Cm remove Ar install.id
+.Nm
+.Cm find-garbage
 .\" ----------------------------------------------------------------
 .Sh DESCRIPTION
 The
@@ -44,6 +46,7 @@ The
 utility lists
 .Xr freebsd-update 8
 rollback points and removes them.
+It can also scan the storage directory to find unreferenced objects.
 .Pp
 The following commands are available.
 .Bl -tag -width "remove"
@@ -55,6 +58,9 @@ Remove a rollback point with the given ID.
 The point must be the oldest one.
 Removing all points is not allowed;
 in other words, the most recent point cannot be removed.
+.It Cm find-garbage
+Scan the chain of rollback points to find unreferenced objects.
+This command itself does not delete files.
 .El
 .Ss List Format
 The

commit a627469d7f09a42779672669bb721899ef59cc5a

Fix a bug in "remove" command; old files are not deleted at all.
This bug was introduced with commit 55f73d6.

freebsd-update-gc.sh
@@ -104,8 +104,8 @@ check_if_last_rp() {
 	echo "$prev"
 }
 
-# List object hashes.  Each hash is followed by the newest rollback point
-# that refers the object.
+# List object hashes (+ trailing .gz).  Each hash is followed by the
+# newest rollback point that refers the object.
 get_newest_ref() {
 	local rp rollback_list find_garbage
 
@@ -220,7 +220,7 @@ remove() {
 	$doit touch -d $mtime "$DBDIR/$prev_rp"
 	$doit rm "$DBDIR/$rp"/*
 	$doit rmdir "$DBDIR/$rp"
-	sed -n "/$rp\$/ { s| .*$|.gz|; s|^|$DBDIR/files/|; p; }" <<-EOS | \
+	sed -n "/ $rp\$/ { s| .*\$||; s|^|$DBDIR/files/|; p; }" <<-EOS | \
 	    $doit xargs rm
 		$newest_ref
 	EOS